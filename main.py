d36deac48b72d266464bfc6cc75da054c113ca3b
xpandeyed <lalbiharipandeyg@gmail.com>
2022-01-24 16:11:04 +0530

    Python execution added.

--- main.py
+++ main.py
@@ -8,3 +8,13 @@ print("multiple times printer || " * 4)
 # 3. IronPython (in C#)
 # 4. PyPy (Subset of python)
 
+# C program -> C Compiler -> Machine Code (Readable by Machine)
+# In case of C, the machine code is machine specific (actually CPU specific) and machine code for one operating system will not work for another operating system
+
+# Java program -> Java Compiler -> Java bytecode -> Java Virtual Machine -> Machine Code
+# Java virtual machine converts the bytecode into respective machine code
+
+# C# and CPython work the save way as java
+
+# We can use Java in Python (Jython implementaion not in CPython)
+# In case of Jython the end result is not Python bytecode, it is Java bytecode
