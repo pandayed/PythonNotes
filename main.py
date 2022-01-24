d0ae01eaed2e5708f6dc6f1b82f5ebe072db1e34
xpandeyed <lalbiharipandeyg@gmail.com>
2022-01-24 15:40:32 +0530

    Implementations of python added.

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
 print("Hello World")
+print("multiple times printer || " * 4)
+
+# comments start with hash
+# Python has several implementations:
+# 1. CPython (default implementation) (in C)
+# 2. Jython (in Java)
+# 3. IronPython (in C#)
+# 4. PyPy (Subset of python)
+
