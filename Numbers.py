afd8a83b1625a439ce006947cd4645a7818846de
xpandeyed <lalbiharipandeyg@gmail.com>
2022-02-04 15:49:06 +0530

    Numbers added.

---
+++ Numbers.py
@@ -0,0 +1,18 @@
+x = 10
+
+# 0b prefix is used for binary representation
+y = 0b101
+print(y)
+
+# bin function gives binary representation of a number
+print(bin(x))
+
+# 0x for hexadecimals
+z = 0x12c
+
+# hex fun returns hex representation
+print(hex(x))
+
+# complex numbers
+a = 1+2j # j or J represents imaginary part
+print(a)
