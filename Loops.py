29dce3a805dc6258b2118606dde61c19da167fee
xpandeyed <lalbiharipandeyg@gmail.com>
2022-02-04 22:46:24 +0530

    For loops added.

---
+++ Loops.py
@@ -0,0 +1,28 @@
+# for loop
+# we can iterate over anything that is iterable
+for x in "Python":
+    print(x)
+
+for x in [10, 20, 30]:
+    print(x)
+
+# 0 to 4
+for x in range(5):
+    print(x)
+
+# 2 to 9
+for x in range(2, 10):
+    print(x)
+
+# 0 to 10 with step 2
+for x in range(0, 10, 2):
+    print(x)
+
+# range function
+print(range(5))
+# range function returns range object
+print(type(range(5)))
+# directly printing list
+print([1, 2, 3, 4, 5])
+
+
