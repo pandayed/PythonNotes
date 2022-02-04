135c45606ba50c84bb809314547b43ca7df3e3cc
xpandeyed <lalbiharipandeyg@gmail.com>
2022-02-04 15:54:39 +0530

    Arithmetic operators added.

--- Numbers.py
+++ Numbers.py
@@ -16,3 +16,31 @@ print(hex(x))
 # complex numbers
 a = 1+2j # j or J represents imaginary part
 print(a)
+
+# Arithmetic Operators
+x = 10 + 20
+print(x)
+x = 10 - 2
+print(x)
+x = 10 * 2
+print(x)
+x = 10 % 2
+print(x)
+
+# there are two types of division
+x = 10/3 # gives decimal answer
+print(x)
+x = 10 // 3 # gives int answer
+print(x)
+
+# left to the power of right
+x = 10 ** 2
+print(x)
+
+# augmented assignment operators
+x+=1
+x-=1
+x/=2
+x*=2
+
+# in python we do not have increment and decrement operators
