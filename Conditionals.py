5c3e348708b3af2f0aa85907e4eecfe2861d41bc
xpandeyed <lalbiharipandeyg@gmail.com>
2022-02-04 22:35:38 +0530

    Logical operators added.

--- Conditionals.py
+++ Conditionals.py
@@ -13,3 +13,24 @@ if x>1:
     pass
 else:
     print("do something")
+
+# logical operators
+# and
+# or
+# not
+x = 100
+y = 20
+if not x>y:
+    print("x is not greater than y")
+else:
+    print("x is greater than y")
+
+if x>y and x>10:
+    print("x is greater than y and x is greater than 10")
+
+if x>y or x>10:
+    print("x is greater than y or x is greater than 10")
+
+# chaining of operators
+if 10<x<20:
+    print("x lies between 10 and 20")
