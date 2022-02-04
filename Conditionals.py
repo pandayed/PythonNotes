8ab328c8464f0c44b8f6f6df8e2aaebff2521b87
xpandeyed <lalbiharipandeyg@gmail.com>
2022-02-04 21:15:06 +0530

    Conditionals added.

---
+++ Conditionals.py
@@ -0,0 +1,15 @@
+age = 19
+if age>=18:
+    print("Adult")
+elif age>=45:
+    print("Too Adult")
+else:
+    print("Whatever but not adult")
+
+# in python we cannot use tabs in place of spaces and vice versa
+x=10
+# if block can not be empty so we use pass keyword
+if x>1:
+    pass
+else:
+    print("do something")
