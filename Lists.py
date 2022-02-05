ccb167d3c7e8a807c6ae18dc4e0ec1171dec0d81
xpandeyed <lalbiharipandeyg@gmail.com>
2022-02-05 21:38:27 +0530

    Lists added.

---
+++ Lists.py
@@ -0,0 +1,26 @@
+# lists are enclosed in []
+names = ["lal", "bihari", "pandey"]
+
+# list of lists
+matrix = [[1, 2, 4], [3], [4,5]]
+
+# list of 5 zeroes
+zeros = [0]*5
+print(zeros)
+
+# lists can have mutliple datatype items
+list1 = [10, "a"]
+
+# conatenating lists
+combination = list1 + zeros
+print(combination)
+
+# list function: it takes an iterable
+list3 = list(range(2, 10))
+print(list3)
+
+mychars = list("Lal Bihari Pandey")
+print(mychars)
+
+# len function
+print(len(mychars))
