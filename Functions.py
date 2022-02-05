578e23b44d92fb8fd0f091eeea8fa98cbc3410f9
xpandeyed <lalbiharipandeyg@gmail.com>
2022-02-05 21:14:41 +0530

    Scope of variables added.

--- Functions.py
+++ Functions.py
@@ -67,3 +67,36 @@ def myFullName(**names):
 
 
 myFullName(fName="Lal", mName="Bihari", lName = "Pandey")
+
+
+# there are two types of scope in python : local and global
+# local variables
+# if a variable is defined in a function it is accessible in the function after its definition
+# there is nothing like block scope
+def localVariableExample():
+    if 10<20:
+        localVariable = "Some message"
+
+    if False:
+        localVar2 = "Message"
+    # locanVar 2 is not defined actually so it is not accessible outside
+    print(localVariable)
+    # print(localVar2) # this will generate error
+
+
+localVariableExample()
+
+
+var1 = "Var1"
+var2 = "Var2"
+def variableScopeExample():
+    # a new var1 is created for this fun
+    var1 = "Var 1 of fun"
+
+    # this is the global var2
+    global var2
+    var2 = "Var 2 in fun but global"
+
+
+# modifying global variables inside functions is not a good practice because other fun may depend on original value
+# so do not use global statement in functions
