ddcd5a953b680a2ccb01c18ecef686a29ed92dd1
xpandeyed <lalbiharipandeyg@gmail.com>
2022-02-16 22:23:50 +0530

    Swap two variables added.

---
+++ SwapTwoVariables.py
@@ -0,0 +1,10 @@
+# In python it is often asked question
+x = 10
+y = 20
+x, y = y, x
+# this is same as
+x, y = (20, 10)
+# when we write comma separated values/variables of right of '=' and some variables on left
+# we are basically defining tuple on right
+# and unpacking it on left
+a, b = 10, 20
